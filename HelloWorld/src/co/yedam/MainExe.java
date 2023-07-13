da0a138cc0ac3e3bda1df35f0f476baa5402d807
admin <admin@DESKTOP-PG5RNBB>
2023-07-13 15:40:45 +0900

    first commit

---
+++ HelloWorld/src/co/yedam/MainExe.java
@@ -0,0 +1,12 @@
+package co.yedam;
+
+public class MainExe {
+	public static void main(String[] args) {
+		System.out.println("hello World"); 
+		System.out.println("jdk compile TEST");
+		
+		PrintExe printExe = new PrintExe();;
+		printExe.showInfo();
+		
+	}
+}
